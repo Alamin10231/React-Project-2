9c5a8b33f526a4eeb2906309badb275a60f276fa
Md Alamin <alamin3636md@gmail.com>
2024-09-02 15:12:59 +0600

    again try

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navigation() {
+  return (
+    <nav>
+        <div>
+            <img src="./images/logo.png" alt="" />
+        </div>
+        <ul>
+            <li>HOME</li>
+            <li>ABOUT</li>
+            <li>CONTACT US</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navigation
